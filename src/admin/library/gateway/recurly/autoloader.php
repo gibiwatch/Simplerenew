db31379d64d51d8f2f373dc52004fc1f2d6276f7
Bill Tomczak <bill@ostraining.com>
2014-05-07 12:20:37 -0700

    Create autoloader for recurly classes

---
+++ src/admin/library/gateway/recurly/autoloader.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package   com_simplerenew
+ * @contact   www.ostraining.com, support@ostraining.com
+ * @copyright 2014 Open Source Training, LLC. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+
+defined('_JEXEC') or die();
+
+RecurlyLoader::register();
+
+abstract class RecurlyLoader
+{
+    protected static $exceptions = array(
+        'Recurly_CouponRedemption' => 'redemption.php',
+        'Recurly_ClientResponse'   => 'response.php',
+        'Recurly_HmacHash'         => 'util/hmac_hash.php'
+    );
+
+    public static function register()
+    {
+        spl_autoload_register(array('\\RecurlyLoader', 'load'), true);
+    }
+
+    protected static function load($class)
+    {
+        if ( !class_exists($class) && strpos($class, 'Recurly_') === 0 ) {
+            if (array_key_exists($class, self::$exceptions)) {
+                $file = self::$exceptions[$class];
+            } else {
+                list(,$file) = explode('_', $class);
+                $parts = preg_split('/(?<=[a-z])(?=[A-Z])/x', $file);
+                $file = strtolower(join('_', $parts)) . '.php';
+            }
+            $file = __DIR__ . '/recurly/' . $file;
+
+            if ( file_exists($file) ) {
+                require_once $file;
+            }
+        }
+    }
+}
