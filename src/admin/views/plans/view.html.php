9c482d97346ac718666aebd0dd8a417708ab4e09
Bill Tomczak <bill@ostraining.com>
2014-06-02 15:38:45 -0700

    Add skeleton for plans admin view

---
+++ src/admin/views/plans/view.html.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package   com_simplerenew
+ * @contact   www.ostraining.com, support@ostraining.com
+ * @copyright 2014 Open Source Training, LLC. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+
+defined('_JEXEC') or die();
+
+class SimplerenewViewPlans extends SimplerenewAdminView
+{
+    public function display($tpl = null)
+    {
+        $this->setToolbar();
+        parent::display($tpl);
+    }
+
+    protected function setToolbar()
+    {
+        $this->setTitle();
+    }
+}
