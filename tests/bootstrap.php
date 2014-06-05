06336a6cd156fecb2cfa5e7f9bd6ca5e1a748aa9
Anderson Grüdtner Martins <andergmartins@gmail.com>
2014-06-05 10:30:46 -0300

    Add file comments

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,12 @@
 <?php
 /**
+ * @package   tests_simplerenew
+ * @contact   www.ostraining.com, support@ostraining.com
+ * @copyright 2014 Open Source Training, LLC. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+
+/**
  * Loads the necessary support for testing Simple Renew
  */
 
