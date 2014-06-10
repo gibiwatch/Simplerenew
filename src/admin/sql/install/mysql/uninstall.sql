31f4c1c0335699c607eab3e5b6150004752cfb7a
Bill Tomczak <bill@ostraining.com>
2014-06-10 15:39:35 -0700

    Update tables for native Joomla coding. Clear out tables we don't intend to use

--- src/admin/sql/install/mysql/uninstall.sql
+++ src/admin/sql/install/mysql/uninstall.sql
@@ -1,6 +1,2 @@
-DROP TABLE IF EXISTS `#__sr_accounts` ;
-DROP TABLE IF EXISTS `#__sr_billing` ;
-DROP TABLE IF EXISTS `#__sr_coupons` ;
-DROP TABLE IF EXISTS `#__sr_plans` ;
-DROP TABLE IF EXISTS `#__sr_subscriptions` ;
-DROP TABLE IF EXISTS `#__sr_coupons_plans` ;
+DROP TABLE IF EXISTS `#__simplerenew_groups`;
+DROP TABLE IF EXISTS `#__simplerenew_plans` ;
