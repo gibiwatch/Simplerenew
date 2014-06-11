263b309fc3fd7a25360854e8dcea395afce9ae15
Bill Tomczak <bill@ostraining.com>
2014-06-11 11:21:00 -0700

    Reorganize config.xml for J3.x eccentricities

--- src/admin/models/fields/heading.php
+++ src/admin/models/fields/heading.php
@@ -18,10 +18,11 @@ class JFormFieldHeading extends JFormField
     public function getLabel()
     {
         $tag = $this->element['tag'] ? (string)$this->element['tag'] : 'p';
+        $text = JText::_((string)$this->element['label']);
 
         return '<' . $tag
             . ' style="clear: both; padding: 10px 0px; margin: 0;">'
-            . $this->element['label']
+            . $text
             . '</' . $tag . '>';
     }
 }
