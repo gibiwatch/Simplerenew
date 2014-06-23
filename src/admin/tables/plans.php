f3c48e34a07cd5f8165bae05f40f5f7ec787fa49
Bill Tomczak <bill@ostraining.com>
2014-06-23 11:54:16 -0700

    Prevent duplicate plan code or alias

--- src/admin/tables/plans.php
+++ src/admin/tables/plans.php
@@ -18,7 +18,7 @@ class SimplerenewTablePlans extends SimplerenewTable
         parent::__construct('#__simplerenew_plans', 'id', $db);
     }
 
-    public function check()
+    public function store($updateNulls = false)
     {
         if (trim($this->alias) == '') {
             $this->alias = $this->code;
@@ -26,6 +26,32 @@ class SimplerenewTablePlans extends SimplerenewTable
 
         $this->alias = SimplerenewApplicationHelper::stringURLSafe($this->alias);
 
-        return true;
+        // Verify that the code and alias are unique
+        $db = $this->getDbo();
+        $query = $db->getQuery(true)
+            ->select('count(*)')
+            ->from($this->_tbl);
+
+        if ($this->id) {
+            $query->where('id <> ' . $db->quote($this->id));
+        }
+        $query->where(
+            '('
+            . join(
+                ' OR ',
+                array(
+                    'alias = ' . $db->quote($this->alias),
+                    'code = ' . $db->quote($this->code)
+                )
+            )
+            . ')'
+        );
+
+        if ($db->setQuery($query)->loadResult() > 0) {
+            $this->setError(JText::_('COM_SIMPLERENEW_ERROR_PLAN_DUPLICATE'));
+            return false;
+        }
+
+        return parent::store($updateNulls);
     }
 }
