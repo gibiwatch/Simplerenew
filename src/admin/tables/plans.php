8bdf161af9ffe868496965bac885d14d06b102bf
Bill Tomczak <bill@ostraining.com>
2014-06-23 13:20:15 -0700

    Add saving to gateway to plan table

--- src/admin/tables/plans.php
+++ src/admin/tables/plans.php
@@ -6,16 +6,36 @@
  * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
 
+use Simplerenew\Api\Plan;
+
 defined('_JEXEC') or die();
 
 class SimplerenewTablePlans extends SimplerenewTable
 {
+    /**
+     * @var Plan
+     */
+    protected $_gateway = null;
+
     /**
      * @param JDatabaseDriver $db
      */
     public function __construct(&$db)
     {
         parent::__construct('#__simplerenew_plans', 'id', $db);
+    }
+
+    public function setGateway(Plan $plan)
+    {
+        $this->_gateway = $plan;
+    }
+
+    public function storeGateway()
+    {
+        if (!$this->_gateway instanceof Plan) {
+            $this->_gateway = SimplerenewHelper::getSimplerenew()->getPlan();
+        }
+        return $this->_gateway->save(true);
     }
 
     public function store($updateNulls = false)
@@ -49,6 +69,11 @@ class SimplerenewTablePlans extends SimplerenewTable
 
         if ($db->setQuery($query)->loadResult() > 0) {
             $this->setError(JText::_('COM_SIMPLERENEW_ERROR_PLAN_DUPLICATE'));
+            return false;
+        }
+
+        if (!$this->storeGateway()) {
+            $this->setError(JText::_('COM_SIMPLERENEW_ERROR_PLAN_GATEWAY_SAVE'));
             return false;
         }
 
