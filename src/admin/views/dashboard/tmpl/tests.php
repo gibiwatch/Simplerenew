4bdcd63ca06450845242a7a4d3d61e50f7c0b504
Bill Tomczak <bill@ostraining.com>
2014-06-24 16:16:45 -0700

    update dashboard tests

--- src/admin/views/dashboard/tmpl/tests.php
+++ src/admin/views/dashboard/tmpl/tests.php
@@ -11,17 +11,17 @@ defined('_JEXEC') or die();
 try {
     $sr = SimplerenewHelper::getSimplerenew();
 
-    $plan = $sr->getPlan();
-//    $user = $sr->getUser()->loadByUsername('bill');
-//    $user2 = $sr->getUser()->loadByUsername('fred');
-
-//    $account = $sr->getAccount()->load($user);
-//    $billing = $sr->getBilling()->load($account);
-
     echo '<pre>';
-    echo str_pad(' Plan List ', 40, '*', STR_PAD_BOTH) . '<br/>';
+
+    echo str_pad(' Plan ', 40, '*', STR_PAD_BOTH) . '<br/>';
+    $plan = $sr->getPlan()->load('plan-apeforest');
+
+    print_r($plan->getProperties());
+
 
     /** @var Simplerenew\Api\Plan $item */
+/*
+    echo str_pad(' Plan List ', 40, '*', STR_PAD_BOTH) . '<br/>';
     $planList = $plan->getList();
     foreach ($planList as $code => $item) {
         echo '<br/>**' . $code . '<br/>';
@@ -29,19 +29,44 @@ try {
         print_r($item->getProperties());
         echo '</pre>';
     }
+*/
 
-//    echo str_pad(' User ', 40, '*', STR_PAD_BOTH) . '<br/>';
-//    print_r($user->getProperties());
-//    print_r($user2->getProperties());
+/*
+    echo str_pad(' User ', 40, '*', STR_PAD_BOTH) . '<br/>';
 
-//    echo str_pad(' Account ', 40, '*', STR_PAD_BOTH) . '<br/>';
-//    print_r($account->getProperties());
+    $user = $sr->getUser()->loadByUsername('bill');
+    $user2 = $sr->getUser()->loadByUsername('fred');
 
-//    echo str_pad(' Billing ', 40, '*', STR_PAD_BOTH) . '<br/>';
-//    print_r($billing->getProperties());
-//    print_r($billing->payment->getProperties());
+    print_r($user->getProperties());
+    print_r($user2->getProperties());
+*/
 
-        echo '</pre>';
+/*
+    echo str_pad(' Account ', 40, '*', STR_PAD_BOTH) . '<br/>';
+    $user = $sr->getUser()->loadByUsername('bill');
+    $user->id = 976;
+    $account = $sr->getAccount()->load($user);
+
+    print_r($account->getProperties());
+
+
+    echo str_pad(' Billing ', 40, '*', STR_PAD_BOTH) . '<br/>';
+//    $user = $sr->getUser()->loadByUsername('bill');
+//    $account = $sr->getAccount()->load($user);
+    $billing = $sr->getBilling()->load($account);
+    print_r($billing->getProperties());
+    print_r($billing->payment->getProperties());
+
+    $billing->getPayment()->setProperties(
+        array(
+            'number' => '4111111111111111',
+            'month' => 12,
+            'year' => 2020
+        )
+    );
+    $billing->save();
+*/
+    echo '</pre>';
 
 } catch (Simplerenew\Exception $e) {
     echo '<br/>SIMPLERENEW: ' . $e->getTraceMessage() . '<br/>';
