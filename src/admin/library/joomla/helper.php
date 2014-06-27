fc8871700c40ae8f79adf41710001e8c54483db3
Bill Tomczak <bill@ostraining.com>
2014-06-27 14:08:48 -0700

    Provide simple helper for temporarily saving form data.

--- src/admin/library/joomla/helper.php
+++ src/admin/library/joomla/helper.php
@@ -26,6 +26,49 @@ abstract class SimplerenewHelper
     }
 
     /**
+     * Save submitted form data in the session.
+     * NOTE: No security is employed here. Caller should be
+     * careful not to store sensitive data in the session.
+     * Avoid storing passwords and credit card data.
+     *
+     * @param string $domain
+     * @param array  $source
+     *
+     * @return void
+     */
+    public static function saveFormData($domain, array $source = null)
+    {
+        $domain = 'simplerenew.' . $domain;
+
+        if ($source === null) {
+            $source = $_REQUEST;
+        }
+        $app = SimplerenewFactory::getApplication();
+
+        $app->setUserState($domain, base64_encode(serialize($source)));
+    }
+
+    /**
+     * Retrieve previously saved form data to repopulate forms
+     *
+     * @param string $domain
+     * @param bool   $clear
+     *
+     * @return array
+     */
+    public static function loadFormData($domain, $clear = true)
+    {
+        $domain = 'simplerenew.' . $domain;
+        $app    = SimplerenewFactory::getApplication();
+        $formData = unserialize(base64_decode($app->getUserState($domain)));
+
+        if ($clear) {
+            $app->setUserState($domain, null);
+        }
+        return $formData;
+    }
+
+    /**
      * @param string $name
      * @param string $link
      * @param bool   $active
