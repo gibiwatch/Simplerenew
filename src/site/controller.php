e8d2589ff78764ae4da1f5ba0b35a2f793fcd26f
Bill Tomczak <bill@ostraining.com>
2014-07-10 10:55:45 -0700

    Remove test code

--- src/site/controller.php
+++ src/site/controller.php
@@ -11,20 +11,4 @@ defined('_JEXEC') or die();
 class SimplerenewController extends SimplerenewControllerBase
 {
     protected $default_view = 'subscribe';
-
-    public function test()
-    {
-        $params = SimplerenewComponentHelper::getParams();
-        $params->set('gateway.recurly.mode', 'live');
-        $params->set('gateway.recurly.liveApikey', '808896419fd94121ba4bbcb0f32f460b');
-        $params->set('gateway.recurly.livePrivate', 'f284ad043e784180b97661881fb459da');
-        $params->set('gateway.recurly.liveSubdomain', 'ostraining');
-
-        $container = SimplerenewFactory::getContainer($params);
-
-        $account = $container->getAccount();
-        $sub = $container->getSubscription();
-
-        $sub->getList($account);
-    }
 }
