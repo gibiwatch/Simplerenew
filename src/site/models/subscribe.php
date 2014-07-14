470a1a9af851aeaf9d66c02d47da4997e19aa989
Bill Tomczak <bill@ostraining.com>
2014-07-14 10:05:10 -0700

    Reorganize model code

--- src/site/models/subscribe.php
+++ src/site/models/subscribe.php
@@ -19,48 +19,60 @@ class SimplerenewModelSubscribe extends SimplerenewModelAccount
             ->select('plans.*')
             ->from('#__simplerenew_plans plans')
             ->order('code');
-
-        if ($plans = $this->getState('filter.plans')) {
-            // Select the plans based on the user's user group
-            $userId = $this->getState('user.id');
-            $user   = SimplerenewFactory::getUser($userId);
-
-            // Start by assuming unsubscribed
-            $checkGroups = array(0);
-            if ($user->id > 0) {
-                // See if the user is in a plan group
-                $planGroups = $db
-                    ->setQuery('Select group_id from #__simplerenew_plans group by group_id')
-                    ->loadColumn();
-
-                $checkGroups = array_intersect($planGroups, $user->groups) ? : $checkGroups;
-            }
-
-            $available = array();
-            foreach ($checkGroups as $group) {
-                if (isset($plans[$group])) {
-                    if (is_array($plans[$group])) {
-                        $available = array_filter(array_merge($available, $plans[$group]));
-                    } elseif ($plans[$group] == '*') {
-                        // This group makes all plans selected
-                        $available = array();
-                        break;
-                    }
-                }
-            }
-
-            if ($available) {
-                $available = array_map(array($db, 'quote'), $available);
-                $query->where('code IN (' . join(',', $available) . ')');
-            } else {
-                return array();
-            }
-        }
-
         $query->where('published = 1');
+
+        if ($available = $this->getAvailable()) {
+            $available = array_map(array($db, 'quote'), $available);
+            $query->where('code IN (' . join(',', $available) . ')');
+        }
 
         $list = $db->setQuery($query)->loadObjectList('code');
         return $list;
+    }
+
+    /**
+     * Get an array of plans being made available to the current user
+     *
+     * @return array
+     */
+    protected function getAvailable()
+    {
+        if ($plans = $this->getState('filter.plans')) {
+            if (!empty($plans['*'])) {
+                // Simple plan selection regardless of user
+                $available = $plans['*'];
+
+            } else {
+                // Select the plans based on the user's user group
+                $userId = $this->getState('user.id');
+                $user   = SimplerenewFactory::getUser($userId);
+
+                // Start by assuming unsubscribed
+                $checkGroups = array(0);
+                if ($user->id > 0) {
+                    // See if the user is in a plan group
+                    $planGroups = SimplerenewFactory::getDbo()
+                        ->setQuery('Select group_id from #__simplerenew_plans group by group_id')
+                        ->loadColumn();
+
+                    $checkGroups = array_intersect($planGroups, $user->groups) ? : $checkGroups;
+                }
+
+                $available = array();
+                foreach ($checkGroups as $group) {
+                    if (isset($plans[$group])) {
+                        if (is_array($plans[$group])) {
+                            $available = array_filter(array_merge($available, $plans[$group]));
+                        } elseif ($plans[$group] == '*') {
+                            // This group makes all plans selected
+                            $available = array();
+                            break;
+                        }
+                    }
+                }
+            }
+            return $available;
+        }
     }
 
     protected function populateState()
