ce5feeb275c0ac3c7fbf40c31772fe60fa32dadc
Bill Tomczak <bill@ostraining.com>
2014-07-15 18:57:04 -0700

    Issue #54 - create plan edit form

--- src/admin/views/plan/tmpl/edit.j2.php
+++ src/admin/views/plan/tmpl/edit.j2.php
@@ -7,30 +7,95 @@
  */
 
 defined('_JEXEC') or die();
+
+/**
+ * @var SimplerenewViewPlan $this
+ * @var JFormField          $field
+ */
+
+JHtml::_('behavior.formvalidation');
+JHtml::_('behavior.keepalive');
+
+$app = SimplerenewFactory::getApplication();
+$input = $app->input;
+$fieldSets = $this->form->getFieldsets();
+
+$style = <<<CSS
+div.inline label {
+    clear: none;
+    min-width: 0px;
+}
+CSS;
+SimplerenewFactory::getDocument()->addStyleDeclaration($style);
+
 ?>
+<script>
+    Joomla.submitbutton = function (task) {
+        if (task == 'plan.cancel' || document.formvalidator.isValid(document.id('item-form'))) {
+            <?php echo $this->form->getField('description')->save(); ?>
+            Joomla.submitform(task, document.getElementById('item-form'));
+        }
+    }
+</script>
 <form
-    action="<?php echo 'index.php?option=com_simplerenew&view=plan&layout=edit&id=' . (int)@$this->item->id; ?>"
+    action="<?php echo JRoute::_('index.php?option=com_simplerenew&layout=edit&id=' . (int)$this->item->id); ?>"
     method="post"
     name="adminForm"
     id="item-form"
     class="form-validate">
 
-    <div class="width-50 fltlft">
-        <fieldset class="adminform">
-            <legend>
-                Joomla 2.x not yet supported
-            </legend>
+    <div class="width-100 inline">
+        <fieldset class="adminform fltlft">
+            <?php
+            echo $this->form->getLabel('code');
+            echo $this->form->getInput('code');
+            echo $this->form->getLabel('alias');
+            echo $this->form->getInput('alias');
+            ?>
+        </fieldset>
+    </div>
+    <div class="clr"></div>
 
+    <?php echo JHtml::_('tabs.start', 'plans-pane'); ?>
+
+    <?php
+    echo JHtml::_(
+        'tabs.panel',
+        JText::_($fieldSets['main']->label),
+        $fieldSets['main']->name . '-page'
+    );
+    ?>
+    <div class="width-100">
+        <fieldset class="adminform fltlft">
             <ul class="adminformlist">
-                <li></li>
-
+                <?php
+                $mainFields = $this->form->getFieldset('main');
+                foreach ($mainFields as $field) {
+                    echo '<li>' . $field->label . $field->input . '</li>';
+                }
+                ?>
             </ul>
         </fieldset>
     </div>
+    <div class="clr"></div>
 
-    <input
-        type="hidden"
-        name="task"
-        value=""/>
+    <?php
+    echo JHtml::_(
+        'tabs.panel',
+        JText::_('COM_SIMPLERENEW_PLAN_DESCRIPTION_LABEL'),
+        'description-page'
+    );
+    ?>
+    <div class="width-100">
+        <fieldset class="adminform">
+            <?php echo $this->form->getInput('description'); ?>
+        </fieldset>
+    </div>
+    <div class="clr"></div>
+
+    <?php echo JHtml::_('tabs.end'); ?>
+
+    <input type="hidden" name="task" value=""/>
+    <input type="hidden" name="return" value="<?php echo $input->getCmd('return'); ?>"/>
     <?php echo JHtml::_('form.token'); ?>
 </form>
