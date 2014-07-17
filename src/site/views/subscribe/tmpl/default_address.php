a0d7f15e2bdebaedf20f4ad864dec882aeeb1171
Bill Tomczak <bill@ostraining.com>
2014-07-16 19:33:00 -0700

    Add address selections and start usage on subscribe screen

--- src/site/views/subscribe/tmpl/default_address.php
+++ src/site/views/subscribe/tmpl/default_address.php
@@ -9,72 +9,84 @@
 defined('_JEXEC') or die();
 
 /** @var SimplerenewViewSubscribe $this */
-?>
-<div class="ost-section">
-    <div class="block6">
-        <label for="billing_address1"><?php echo JText::_('COM_SIMPLERENEW_BILLING_ADDRESS1'); ?></label>
-        <input
-            id="billing_address1"
-            name="billing[address1]"
-            type="text"
-            value="<?php echo $this->billing->address->address1; ?>"
-            required="true"/>
-    </div>
-    <div class="block6">
-        <label for="billing_address2"><?php echo JText::_('COM_SIMPLERENEW_BILLING_ADDRESS2'); ?></label>
-        <input
-            id="billing_address2"
-            name="billing[address2]"
-            type="text"
-            value="<?php echo $this->billing->address2; ?>"
-            required="true"/>
-    </div>
-</div>
-<!-- /.ost-section -->
 
-<div class="ost-section">
-    <div class="block6">
-        <label for="billing_city"><?php echo JText::_('COM_SIMPLERENEW_BILLING_CITY'); ?></label>
-        <input
-            id="billing_city"
-            name="billing[city]"
-            type="text"
-            value="<?php echo $this->billing->address->city; ?>"
-            required="true"/>
-    </div>
-    <div class="block6">
-        <label for="billing_region"><?php echo JText::_('COM_SIMPLERENEW_BILLING_REGION'); ?></label>
-        <input
-            id="billing_region"
-            name="billing[region]"
-            type="text"
-            value="<?php echo $this->billing->address->region; ?>"
-            required="true"/>
-    </div>
-</div>
-<!-- /.ost-section -->
+$required = $this->getParams()->get('account.billingAddress');
 
-<div class="ost-section">
-    <div class="block6">
-        <label for="billing_postal"><?php echo JText::_('COM_SIMPLERENEW_BILLING_POSTAL'); ?></label>
-        <input
-            id="billing_postal"
-            name="billing[postal]"
-            type="text"
-            value="<?php echo $this->billing->address->postal; ?>"
-            required="true"/>
+$fields = array();
+if ($required != 'none') {
+    $names    = array_keys($this->billing->address->getProperties());
+    $required = !$required ? $names : array_intersect($names, explode(',', $required));
+    $fields   = $required;
+}
+if ($fields):
+    ?>
+    <div class="ost-section">
+        <div class="block6">
+            <label for="billing_address1"><?php echo JText::_('COM_SIMPLERENEW_BILLING_ADDRESS1'); ?></label>
+            <input
+                id="billing_address1"
+                name="billing[address1]"
+                type="text"
+                value="<?php echo $this->billing->address->address1; ?>"
+                required="true"/>
+        </div>
+        <div class="block6">
+            <label for="billing_address2"><?php echo JText::_('COM_SIMPLERENEW_BILLING_ADDRESS2'); ?></label>
+            <input
+                id="billing_address2"
+                name="billing[address2]"
+                type="text"
+                value="<?php echo $this->billing->address2; ?>"
+                required="true"/>
+        </div>
     </div>
-    <div class="block6">
-        <label for="billing_country"><?php echo JText::_('COM_SIMPLERENEW_BILLING_COUNTRY'); ?></label>
-        <?php
-        echo JHtml::_(
-            'srselect.country',
-            'billing[country]',
-            'required="true"',
-            $this->billing->address->country,
-            'billing_country'
-        );
-        ?>
+    <!-- /.ost-section -->
+
+    <div class="ost-section">
+        <div class="block6">
+            <label for="billing_city"><?php echo JText::_('COM_SIMPLERENEW_BILLING_CITY'); ?></label>
+            <input
+                id="billing_city"
+                name="billing[city]"
+                type="text"
+                value="<?php echo $this->billing->address->city; ?>"
+                required="true"/>
+        </div>
+        <div class="block6">
+            <label for="billing_region"><?php echo JText::_('COM_SIMPLERENEW_BILLING_REGION'); ?></label>
+            <input
+                id="billing_region"
+                name="billing[region]"
+                type="text"
+                value="<?php echo $this->billing->address->region; ?>"
+                required="true"/>
+        </div>
     </div>
-</div>
-<!-- /.ost-section -->
+    <!-- /.ost-section -->
+
+    <div class="ost-section">
+        <div class="block6">
+            <label for="billing_postal"><?php echo JText::_('COM_SIMPLERENEW_BILLING_POSTAL'); ?></label>
+            <input
+                id="billing_postal"
+                name="billing[postal]"
+                type="text"
+                value="<?php echo $this->billing->address->postal; ?>"
+                required="true"/>
+        </div>
+        <div class="block6">
+            <label for="billing_country"><?php echo JText::_('COM_SIMPLERENEW_BILLING_COUNTRY'); ?></label>
+            <?php
+            echo JHtml::_(
+                'srselect.country',
+                'billing[country]',
+                'required="true"',
+                $this->billing->address->country,
+                'billing_country'
+            );
+            ?>
+        </div>
+    </div>
+    <!-- /.ost-section -->
+<?php
+endif;
