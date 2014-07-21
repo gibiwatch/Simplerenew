4337e39bd32c1630d4497fbe33a440c8cbaebe4f
Bill Tomczak <bill@ostraining.com>
2014-07-21 12:11:54 -0700

    Add jQuery loading method

--- src/admin/library/html/sr.php
+++ src/admin/library/html/sr.php
@@ -10,6 +10,13 @@ defined('_JEXEC') or die();
 
 abstract class JHtmlSr
 {
+    protected static $jqueryLoaded = array();
+
+    /**
+     * Generate link to Terms & Conditions page
+     *
+     * @return string
+     */
     public static function terms()
     {
         $params = SimplerenewComponentHelper::getParams();
@@ -25,5 +32,45 @@ abstract class JHtmlSr
             return JText::sprintf('COM_SIMPLERENEW_TERMS_OF_AGREEMENT', $link);
         }
         return '';
+    }
+
+    /**
+     * Load jQuery core
+     *
+     * @param bool $noConflict
+     * @param bool $debug
+     */
+    public static function jquery($noConflict = true, $debug = null)
+    {
+        $params = SimplerenewComponentHelper::getParams();
+
+        if ($params->get('advanced.jquery', 1)) {
+            if (version_compare(JVERSION, '3', 'ge')) {
+                    JHtml::_('jquery.framework', $noConflict, $debug);
+                    return;
+            }
+
+            // pre 3.0 manual loading
+
+            // Only load once
+            if (!empty(static::$jqueryLoaded[__METHOD__])) {
+                return;
+            }
+
+            // If no debugging value is set, use the configuration setting
+            if ($debug === null) {
+                $config = JFactory::getConfig();
+                $debug  = (boolean)$config->get('debug');
+            }
+
+            JHtml::_('script', 'com_simplerenew/jquery.js', false, true, false, false, $debug);
+
+            // Check if we are loading in noConflict
+            if ($noConflict) {
+                JHtml::_('script', 'com_simplerenew/jquery-noconflict.js', false, true, false, false, false);
+            }
+
+            static::$jqueryLoaded[__METHOD__] = true;
+        }
     }
 }
