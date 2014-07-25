fc613c4b8ca56b7bc5c8a4c482e078ca1e29ad40
Bill Tomczak <bill@ostraining.com>
2014-07-25 12:47:45 -0700

    improve payment method abstraction

---
+++ src/site/views/subscribe/tmpl/default_paypal.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package   Simplerenew
+ * @contact   www.ostraining.com, support@ostraining.com
+ * @copyright 2014 Open Source Training, LLC. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+
+defined('_JEXEC') or die();
+?>
+<div class="ost-section" id="content_paypal">
+    <div class="block12">
+        <div class="p-full">
+            <?php echo JText::_('COM_SIMPLERENEW_PROCEED_TO_PAYPAL'); ?>
+            <input type="hidden" name="payment_method" value="pp"/>
+        </div>
+    </div>
+</div>
+<!-- /.ost-section -->
