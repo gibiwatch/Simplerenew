eae2cbb9c421845e47b3c6f6b6e8ba1897bd0c2e
Bill Tomczak <bill@ostraining.com>
2014-08-01 21:23:06 -0700

    Add processing for subscription changes

---
+++ src/admin/library/simplerenew/Notify/Handler/Subscription.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package   Simplerenew
+ * @contact   www.ostraining.com, support@ostraining.com
+ * @copyright 2014 Open Source Training, LLC. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+
+namespace Simplerenew\Notify\Handler;
+
+use Simplerenew\Exception;
+use Simplerenew\Notify\Notify;
+
+defined('_JEXEC') or die();
+
+class Subscription implements HandlerInterface
+{
+    /**
+     * @param Notify $notice
+     *
+     * @return mixed
+     */
+    public function execute(Notify $notice)
+    {
+        if ($notice->user->id) {
+            switch ($notice->action) {
+                case Notify::ACTION_NEW:
+                case Notify::ACTION_UPDATE:
+                case Notify::ACTION_RENEW:
+                    $plan = $notice->getContainer()
+                        ->getPlan()
+                        ->load($notice->subscription->plan);
+                    $notice->user->setGroup($plan);
+                    break;
+
+                case Notify::ACTION_EXPIRE:
+                    $notice->user->setGroup();
+                    break;
+
+                    break;
+            }
+        }
+    }
+}
