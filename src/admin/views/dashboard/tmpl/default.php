7eedc028dfe093d591bd43d566762ef104bff828
htmgarcia <htmgarcia@gmail.com>
2014-08-27 17:31:19 -0500

    Load own layout CSS for admin

--- src/admin/views/dashboard/tmpl/default.php
+++ src/admin/views/dashboard/tmpl/default.php
@@ -9,35 +9,47 @@
 defined('_JEXEC') or die();
 
 // Load admin CSS
+JHtml::stylesheet('com_simplerenew/grid.css', null, true);
+JHtml::stylesheet('com_simplerenew/grid-responsive.css', null, true);
 JHtml::stylesheet('com_simplerenew/admin.css', null, true);
 ?>
 
-<div class="row-fluid text-center">
-    <div class="span2">
-        <img src="../media/com_simplerenew/images/logo.png" alt="" />
-    </div>
-    <div class="span1">
-        <img src="../media/com_simplerenew/images/points.png" alt="" />
-    </div>
-    <div class="span2">
-        <img src="../media/com_simplerenew/images/step1.png" alt="" />
-        <p><a href="#">Click the Options button</a></p>
-    </div>
-    <div class="span1">
-        <img src="../media/com_simplerenew/images/raquo.png" alt="" />
-    </div>
-    <div class="span2">
-        <img src="../media/com_simplerenew/images/step2.png" alt="" />
-        <p><a href="#">Enter your Recurly details</a></p>
-    </div>
-    <div class="span1">
-        <img src="../media/com_simplerenew/images/raquo.png" alt="" />
-    </div>
-    <div class="span3">
-        <img src="../media/com_simplerenew/images/step3.png" alt="" />
-        <p><a href="#">Click Save</a></p>
-    </div>
+<div class="alert alert-success">
+    Welcome message goes here.
 </div>
+
+<div class="ost-container">
+    
+    <div class="ost-section ost-steps">
+        <div class="block2">
+            <img src="../media/com_simplerenew/images/logo.png" alt="" />
+        </div>
+        <div class="block1 ost-connector">
+            <img src="../media/com_simplerenew/images/points.png" alt="" />
+        </div>
+        <div class="block2">
+            <img src="../media/com_simplerenew/images/step1.png" alt="" />
+            <p class="ost-step">Click the Options button</p>
+        </div>
+        <div class="block1 ost-connector">
+            <img src="../media/com_simplerenew/images/raquo.png" alt="" />
+        </div>
+        <div class="block2">
+            <img src="../media/com_simplerenew/images/step2.png" alt="" />
+            <p class="ost-step">Enter your Recurly details</p>
+        </div>
+        <div class="block1 ost-connector">
+            <img src="../media/com_simplerenew/images/raquo.png" alt="" />
+        </div>
+        <div class="block2">
+            <img src="../media/com_simplerenew/images/step3.png" alt="" />
+            <p class="ost-step">Click Save</p>
+        </div>
+    </div>
+    <!-- /.ost-section -->
+    
+</div>
+<!-- /.ost-container -->
 
 <p>Under Construction</p>
 <p><a href="index.php?option=com_simplerenew&view=dashboard&layout=tests">Run tests</a></p>
