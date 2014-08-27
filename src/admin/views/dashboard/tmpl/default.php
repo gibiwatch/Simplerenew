76dd4606ae533dcff8e4357f1fdf1266104de6d8
htmgarcia <htmgarcia@gmail.com>
2014-08-27 16:29:22 -0500

    Images and layout for welcome steps

--- src/admin/views/dashboard/tmpl/default.php
+++ src/admin/views/dashboard/tmpl/default.php
@@ -7,6 +7,37 @@
  */
 
 defined('_JEXEC') or die();
+
+// Load admin CSS
+JHtml::stylesheet('com_simplerenew/admin.css', null, true);
 ?>
+
+<div class="row-fluid text-center">
+    <div class="span2">
+        <img src="../media/com_simplerenew/images/logo.png" alt="" />
+    </div>
+    <div class="span1">
+        <img src="../media/com_simplerenew/images/points.png" alt="" />
+    </div>
+    <div class="span2">
+        <img src="../media/com_simplerenew/images/step1.png" alt="" />
+        <p><a href="#">Click the Options button</a></p>
+    </div>
+    <div class="span1">
+        <img src="../media/com_simplerenew/images/raquo.png" alt="" />
+    </div>
+    <div class="span2">
+        <img src="../media/com_simplerenew/images/step2.png" alt="" />
+        <p><a href="#">Enter your Recurly details</a></p>
+    </div>
+    <div class="span1">
+        <img src="../media/com_simplerenew/images/raquo.png" alt="" />
+    </div>
+    <div class="span3">
+        <img src="../media/com_simplerenew/images/step3.png" alt="" />
+        <p><a href="#">Click Save</a></p>
+    </div>
+</div>
+
 <p>Under Construction</p>
 <p><a href="index.php?option=com_simplerenew&view=dashboard&layout=tests">Run tests</a></p>
