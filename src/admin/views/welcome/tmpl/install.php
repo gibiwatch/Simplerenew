134402349b616b4009ec65308dfb887154c93d6e
Valentín <htmgarcia@gmail.com>
2014-09-10 13:33:43 -0500

    Added checkoff layout

--- src/admin/views/welcome/tmpl/install.php
+++ src/admin/views/welcome/tmpl/install.php
@@ -16,11 +16,7 @@ JHtml::stylesheet('com_simplerenew/admin.css', null, true);
 ?>
 
 <div class="alert alert-success">
-    <?php echo JText::_('Use this area to notify everything is done.'); ?>
-</div>
-
-<div class="alert alert-danger">
-    <?php echo JText::_('Use this area to notify something is missing.'); ?>
+    <?php echo JText::_('COM_SIMPLERENEW_WELCOME'); ?>
 </div>
 
 <div class="ost-container">
@@ -34,21 +30,21 @@ JHtml::stylesheet('com_simplerenew/admin.css', null, true);
         </div>
         <div class="block2">
             <img src="../media/com_simplerenew/images/step1.png" alt="" />
-            <p class="ost-step"><?php echo JText::_('Gateway configured'); ?></p>
+            <p class="ost-step"><?php echo JText::_('COM_SIMPLERENEW_WELCOME_OPTIONS'); ?></p>
         </div>
         <div class="block1 ost-connector">
             <img src="../media/com_simplerenew/images/raquo.png" alt="" />
         </div>
         <div class="block2">
             <img src="../media/com_simplerenew/images/step2.png" alt="" />
-            <p class="ost-step"><?php echo JText::_('Plans created'); ?></p>
+            <p class="ost-step"><?php echo JText::_('COM_SIMPLERENEW_WELCOME_DETAILS'); ?></p>
         </div>
         <div class="block1 ost-connector">
             <img src="../media/com_simplerenew/images/raquo.png" alt="" />
         </div>
         <div class="block2">
             <img src="../media/com_simplerenew/images/step3.png" alt="" />
-            <p class="ost-step"><?php echo JText::_('Subscribe view created'); ?></p>
+            <p class="ost-step"><?php echo JText::_('COM_SIMPLERENEW_WELCOME_SAVE'); ?></p>
         </div>
     </div>
     <!-- /.ost-section -->
