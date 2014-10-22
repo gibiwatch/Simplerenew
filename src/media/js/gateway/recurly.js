c7a002ce6a979c0987c97c95d3682cf01994433c
Bill Tomczak <bill@ostraining.com>
2014-10-21 19:50:42 -0700

    Fix calculations for Paypal purchases

--- src/media/js/gateway/recurly.js
+++ src/media/js/gateway/recurly.js
@@ -1,23 +1,54 @@
-(function($) {
+(function ($) {
+    $.extend($.fn, {
+        recurly: {
+            calculate: function (coupon, plan) {
+                var code = $(coupon).is(':disabled') ? '' : $(coupon).val();
+                recurly.Pricing()
+                    .plan($(plan).val())
+                    .coupon(code)
+                    .catch(function (err) {
+                        alert(err.message);
+                    })
+                    .done(function (price) {
+                        $(plan).data('price', {
+                            net: price.now.total,
+                            symbol: price.currency.symbol
+                        })
+                    });
+            }
+        }
+    });
+
     $.Simplerenew.validate.gateway = $.extend({}, $.Simplerenew.validate.gateway, {
         options: {
             key: null
         },
 
-        pricing: function() {
-            alert('gotcha');
-        },
-
-        init: function(form) {
+        init: function (form) {
             if (!this.options.key) {
                 alert('System error: No public key defined');
                 return;
             }
             recurly.configure(this.options.key);
 
+            var coupon = $('#coupon_code')
+                .attr('data-recurly', 'coupon')
+                .on('change sr.disable', function(evt) {
+                    plans.each(function(idx, plan) {
+                        $(plan).recurly.calculate(coupon, plan);
+                    });
+                });
 
-            $('[name^=planCodes]').attr('data-recurly', 'plan');
-            $('#coupon_code').attr('data-recurly', 'coupon');
+            var plans = $('[name^=planCodes]')
+                .attr('data-recurly', 'plan')
+                .on('click', function (evt) {
+                    $(this).recurly.calculate(coupon, this);
+                });
+
+            plans.filter(':checked').each(function(idx, plan) {
+                $(plan).recurly.calculate(coupon, plan);
+            });
+
             $('#billing_cc_number').attr('data-recurly', 'number');
             $('#billing_cc_month').attr('data-recurly', 'month');
             $('#billing_cc_year').attr('data-recurly', 'year');
@@ -32,35 +63,38 @@
             $('#billing_country').attr('data-recurly', 'country');
         },
 
-        submit: function(form) {
+        submit: function (form) {
             var method = $(form).find('input[name=payment_method]:enabled').val();
             var billing_token = $(form).find('#billing_token');
 
             billing_token.val('');
             switch (method) {
                 case 'pp':
-                    var pricing     = recurly.Pricing(),
-                        plan        = $('[data-recurly=plan]:checked'),
-                        coupon      = $('[data-recurly=coupon]:enabled'),
-                        description = plan.attr('data-description') || 'Subscription';
+                    var description, symbol,
+                        total = 0,
+                        items = [],
+                        plans = $('[data-recurly=plan]:checked');
 
-                    pricing
-                        .plan(plan.val())
-                        .coupon(coupon.val())
-                        .catch(function (err) {
-                            alert(err.message);
-                        })
-                        .done(function (price) {
-                            description += ' ' + price.currency.symbol + price.now.total;
-                            recurly.paypal({'description': description}, function(err, token) {
-                                if (err) {
-                                    alert(err.message);
-                                } else {
-                                    billing_token.val(token.id);
-                                    form.submit();
-                                }
-                            });
-                        });
+                    plans.each(function (idx, plan) {
+                        var price = $(plan).data('price');
+                        total += parseFloat(price.net);
+                        symbol = price.symbol;
+                        items.push($(plan).attr('data-description'));
+                    });
+
+                    description = 'Subscription to ' + items.join(', ')
+                        + ' for a total of ' + symbol + total.toFixed(2);
+
+                    recurly.paypal({description: description}, function (err, token) {
+                        if (err) {
+                            if (err.code != 'paypal-canceled') {
+                                alert(err.message);
+                            }
+                        } else {
+                            billing_token.val(token.id);
+                            form.submit();
+                        }
+                    });
                     break;
 
                 case 'cc':
@@ -68,7 +102,7 @@
                     var number = $(form).find('#billing_cc_number').val();
 
                     if (number) {
-                        recurly.token(form, function(err, token) {
+                        recurly.token(form, function (err, token) {
                             if (err) {
                                 alert(err.message);
                             } else {
