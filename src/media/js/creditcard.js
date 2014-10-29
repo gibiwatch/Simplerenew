c24fdf87ddc9561c3ca9b19d57452a48009a4263
Bill Tomczak <bill@ostraining.com>
2014-10-29 15:27:33 -0700

    Issue #251 - ignore spaces as well

--- src/media/js/creditcard.js
+++ src/media/js/creditcard.js
@@ -45,7 +45,7 @@ jQuery.Simplerenew = jQuery.extend({}, jQuery.Simplerenew, {
          * v2.0 11/7/2005
          */
         var Mod10 = function (rawNumb) {
-            var ccNumb = rawNumb.replace(/-/g, '');
+            var ccNumb = rawNumb.replace(/[\s-]/g, '');
             var valid = "0123456789";               // Valid digits in a credit card number
             var len = ccNumb.length;                // The length of the submitted cc number
             var iCCN = parseInt(ccNumb, 10);        // integer of ccNumb
@@ -120,7 +120,7 @@ jQuery.Simplerenew = jQuery.extend({}, jQuery.Simplerenew, {
         };
 
         var verifyCVV = function (rawNumber, cvv) {
-            var ccnumber = rawNumber.replace(/-/g, '');
+            var ccnumber = rawNumber.replace(/[\s-]/g, '');
             var card = getType(ccnumber);
             if (card) {
                 return (cvv >= 1 && cvv <= 9999 && cvv.length == card.cvvlen);
