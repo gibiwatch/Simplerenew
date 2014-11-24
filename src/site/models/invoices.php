fb1427f1d56e463b258448a0c346b9f514611db7
Bill Tomczak <bill@ostraining.com>
2014-11-24 14:01:00 -0800

    Issue #67 - Restrict invoices view to logged in only

---
+++ src/site/models/invoices.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package   Simplerenew
+ * @contact   www.ostraining.com, support@ostraining.com
+ * @copyright 2014 Open Source Training, LLC. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+
+defined('_JEXEC') or die();
+
+require_once __DIR__ . '/account.php';
+
+class SimplerenewModelInvoices extends SimplerenewModelAccount
+{
+}
--- src/site/views/invoices/view.html.php
+++ src/site/views/invoices/view.html.php
@@ -6,8 +6,32 @@
  * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
 
+use Simplerenew\User\User;
+
 defined('_JEXEC') or die();
 
 class SimplerenewViewInvoices extends SimplerenewViewSite
 {
+    /**
+     * @var User
+     */
+    protected $user = null;
+
+    public function display($tpl = null)
+    {
+        /** @var SimplerenewModelInvoices $model */
+        $model = $this->getModel();
+
+        try {
+            $this->user = $model->getUser();
+            if (!$this->user) {
+                $this->setLayout('login');
+            }
+
+        } catch (Simplerenew\Exception $e) {
+            SimplerenewFactory::getApplication()->enqueueMessage($e->getMessage(), 'error');
+        }
+
+        parent::display($tpl);
+    }
 }
