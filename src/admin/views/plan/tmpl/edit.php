852a791f31b40ac8386941f356c5d071ca064b15
Bill Tomczak <bill@ostraining.com>
2014-12-03 19:12:36 -0800

    Issue #214 - Use a more narrative plan edit form

--- src/admin/views/plan/tmpl/edit.php
+++ src/admin/views/plan/tmpl/edit.php
@@ -17,8 +17,11 @@ JHtml::_('behavior.formvalidation');
 JHtml::_('behavior.keepalive');
 JHtml::_('formbehavior.chosen', 'select');
 
+JHtml::stylesheet('com_simplerenew/admin.css', null, true);
+
 $app = SimplerenewFactory::getApplication();
 $input = $app->input;
+
 ?>
 <script type="text/javascript">
     Joomla.submitbutton = function (task) {
@@ -28,26 +31,58 @@ $input = $app->input;
     }
 </script>
 
-<form
-    action="<?php echo JRoute::_('index.php?option=com_simplerenew&layout=edit&id=' . (int)$this->item->id); ?>"
-    method="post"
-    name="adminForm"
-    id="item-form"
-    class="form-validate">
+<div id="ost-custom">
+    <form
+        action="<?php echo JRoute::_('index.php?option=com_simplerenew&layout=edit&id=' . (int)$this->item->id); ?>"
+        method="post"
+        name="adminForm"
+        id="item-form"
+        class="form-validate">
 
-    <div class="form-horizontal">
         <?php
-        echo $this->renderFieldset(
-            'main',
-            array(
-                'length'       => 'unit',
-                'trial_length' => 'trial_unit'
-            )
-        );
+        foreach ($this->form->getFieldset('main') as $field) {
+            echo $field->label;
+        }
         ?>
-    </div>
 
-    <input type="hidden" name="task" value=""/>
-    <input type="hidden" name="return" value="<?php echo $input->getCmd('return'); ?>"/>
-    <?php echo JHtml::_('form.token'); ?>
-</form>
+        <p><?php
+            $code = $this->form->getValue('code');
+            $lang = $code ? 'COM_SIMPLERENEW_NARRATIVE_PLAN_NAMECODE' : 'COM_SIMPLERENEW_NARRATIVE_PLAN_NAME';
+            echo JText::sprintf($lang, $this->form->getInput('name'), $code);
+            ?>
+        </p>
+
+        <p>
+            <?php echo JText::sprintf('COM_SIMPLERENEW_NARRATIVE_PLAN_GROUP', $this->form->getInput('group_id')); ?>
+        </p>
+
+        <p>
+            <?php
+            echo JText::sprintf(
+                'COM_SIMPLERENEW_NARRATIVE_PLAN_FEES',
+                $this->form->getInput('amount'),
+                $this->form->getInput('length'),
+                $this->form->getInput('unit'),
+                $this->form->getInput('trial_length'),
+                $this->form->getInput('trial_unit')
+            );
+            ?>
+        </p>
+
+        <p>
+            <?php echo JText::sprintf('COM_SIMPLERENEW_NARRATIVE_PLAN_SETUP', $this->form->getInput('setup_cost')); ?>
+        </p>
+
+        <p>
+            <?php echo JText::sprintf('COM_SIMPLERENEW_NARRATIVE_PLAN_CURRENCY', $this->form->getValue('currency')); ?>
+        </p>
+
+        <p>
+            <?php echo JText::sprintf('COM_SIMPLERENEW_NARRATIVE_PLAN_STATE', $this->form->getInput('published')); ?>
+        </p>
+
+        <input type="hidden" name="task" value=""/>
+        <input type="hidden" name="return" value="<?php echo $input->getCmd('return'); ?>"/>
+        <?php echo JHtml::_('form.token'); ?>
+    </form>
+</div>
