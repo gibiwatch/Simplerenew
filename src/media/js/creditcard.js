282c153f55ed6b297a96dde75be3c02da850741d
Bill Tomczak <bill@ostraining.com>
2014-12-08 13:36:59 -0800

    Closes #275 - Use a better luhn test and don't exclude non-US cards

--- src/media/js/creditcard.js
+++ src/media/js/creditcard.js
@@ -32,91 +32,36 @@ jQuery.Simplerenew = jQuery.extend({}, jQuery.Simplerenew, {
             return null;
         };
 
-        /*
-         * The following method was found at The JavaScript Source!!
-         * http://www.javascriptsource.com Created by: David Leppek ::
-         * https://www.azcode.com/Mod10
+        /**
+         * Nicely elegant luhn test! Thanks to:
+         * https://gist.github.com/DiegoSalazar/4075533
          *
          * Determine if a credit card number could be valid before submitting it for
          * real-time online authentication. Based on ANSI X4.13, the LUHN formula,
          * aka the Mod 10 algorithm is used to validate accurate credit card
          * numbers.
-         *
-         * v2.0 11/7/2005
          */
-        var Mod10 = function (rawNumb) {
-            var ccNumb = rawNumb.replace(/[\s-]/g, '');
-            var valid = "0123456789";               // Valid digits in a credit card number
-            var len = ccNumb.length;                // The length of the submitted cc number
-            var iCCN = parseInt(ccNumb, 10);        // integer of ccNumb
-            var sCCN = ccNumb.toString();           // string of ccNumb
-            sCCN = sCCN.replace(/^\s+|\s+$/g, '');  // strip spaces
-            var iTotal = 0;                         // integer total set at zero
-            var bNum = true;                        // by default assume it is a number
-            var bResult = false;                    // by default assume it is NOT a valid cc
-            var temp;                               // temp variable for parsing string
-            var calc;                               // used for calculation of each digit
+        var luhnTest = function (value) {
+            // accept only digits, dashes or spaces
+            if (/[^0-9-\s]+/.test(value)) return false;
 
-            // Determine if the ccNumb is in fact all numbers
-            for (var j = 0; j < len; j++) {
-                temp = "" + sCCN.substring(j, j + 1);
-                if (valid.indexOf(temp) == "-1") {
-                    return false;
-                }
-            }
+            // The Luhn Algorithm. It's so pretty.
+            var nCheck = 0, cDigit, nDigit = 0, bEven = false;
+            value = value.replace(/\D/g, "");
 
-            // Determine if it is the proper length
-            if (len === 0) {
-                // nothing, field is blank AND passed above # check
-                return false;
-            }
+            for (var n = value.length - 1; n >= 0; n--) {
+                cDigit = value.charAt(n);
+                nDigit = parseInt(cDigit, 10);
 
-            // ccNumb is a number and the proper length - let's see if it is a valid
-            // card number
-            if (len >= 15) {
-                // 15 or 16 for Amex or V/MC
-                for (var i = len; i > 0; i--) {
-                    calc = parseInt(iCCN, 10) % 10; // right most digit
-                    calc = parseInt(calc, 10);      // assure it is an integer
-                    iTotal += calc;                 // running total of the card number as we loop - Do Nothing to first digit
-                    i--;                            // decrement the count - move to the next digit in the card
-                    iCCN = iCCN / 10;               // subtracts right most digit from ccNumb
-                    calc = parseInt(iCCN, 10) % 10; // NEXT right most digit
-                    calc = calc * 2;                // multiply the digit by two
-
-                    /*
-                     * Instead of some screwy method of converting 16 to a string
-                     * and then parsing 1 and 6 and then adding them to make 7, I
-                     * use a simple switch statement to change the value of calc2 to
-                     * 7 if 16 is the multiple.
-                     */
-                    switch (calc) {
-                        case 10:
-                            calc = 1;
-                            break;       // 5*2=10 & 1+0 = 1
-                        case 12:
-                            calc = 3;
-                            break;       // 6*2=12 & 1+2 = 3
-                        case 14:
-                            calc = 5;
-                            break;       // 7*2=14 & 1+4 = 5
-                        case 16:
-                            calc = 7;
-                            break;       // 8*2=16 & 1+6 = 7
-                        case 18:
-                            calc = 9;
-                            break;       // 9*2=18 & 1+8 = 9
-                        default:         // calc = calc; 4*2 = 8 & 8 = 8 -same for all
-                        // lower numbers
-                    }
-                    iCCN = iCCN / 10;  // subtracts right most digit from ccNum
-                    iTotal += calc;  // running total of the card number as we loop
+                if (bEven) {
+                    if ((nDigit *= 2) > 9) nDigit -= 9;
                 }
 
-                // Could be valid cc# if the sum Mod 10 is zero
-                return ((iTotal % 10) === 0);
+                nCheck += nDigit;
+                bEven = !bEven;
             }
-            return false;
+
+            return (nCheck % 10) == 0;
         };
 
         var verifyCVV = function (rawNumber, cvv) {
@@ -140,7 +85,7 @@ jQuery.Simplerenew = jQuery.extend({}, jQuery.Simplerenew, {
 
         return {
             getType: getType,
-            verifyNumber: Mod10,
+            verifyNumber: luhnTest,
             verifyCVV: verifyCVV,
             verifyDate: verifyDate
         };
