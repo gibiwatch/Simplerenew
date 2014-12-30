cf0c349ed3103bb5338746ac28605f122e03d8d9
Bill Tomczak <bill@ostraining.com>
2014-12-30 14:28:51 -0800

    Closes #280 - delete log entries older than a year ago

--- src/admin/library/simplerenew/Logger.php
+++ src/admin/library/simplerenew/Logger.php
@@ -16,7 +16,7 @@ defined('_JEXEC') or die();
  * Class Logger
  *
  * N.B.!! - this implementation is pretty much wrong. A better system needs to be worked out
- * @TODO: Come up with a decent abstracted logging system
+ * @TODO: Come up with a decent abstracted logging system and/or ORM interface
  *
  * @package Simplerenew
  */
@@ -29,6 +29,14 @@ abstract class Logger
         if (class_exists('JDatabase')) {
             $db = \SimplerenewFactory::getDbo();
             $db->insertObject('#__simplerenew_push_log', $entry, 'id');
+
+            // Trim the log table
+            $cutoff = new \DateTime('-1 year');
+            $query = $db->getQuery(true)
+                ->delete('#__simplerenew_push_log')
+                ->where('DATE(logtime) < ' . $db->quote($cutoff->format('Y-m-d')));
+
+            $db->setQuery($query)->execute();
         }
     }
 }
