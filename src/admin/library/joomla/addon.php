14e05069e555caae50f91d3e0c6c9fc7547d40bf
Bill Tomczak <bill@ostraining.com>
2015-02-11 19:57:15 -0800

    Begin build out of Addon helper class

---
+++ src/admin/library/joomla/addon.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @package   Simplerenew
+ * @contact   www.ostraining.com, support@ostraining.com
+ * @copyright 2014-2015 Open Source Training, LLC. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+
+defined('_JEXEC') or die();
+
+abstract class SimplerenewAddon
+{
+    /**
+     * Register an extension as an addon. Primarily to be used by addons during
+     * installation, but can be used at any time.
+     *
+     * @param string          $title
+     * @param JTableExtension $addon
+     * @param string          $initPath
+     *
+     * @return bool
+     */
+    public static function register($title, JTableExtension $addon, $initPath = null)
+    {
+        $simplerenew = SimplerenewHelper::getExtensionTable();
+
+        if ($addon->type == 'component') {
+            $initPath = $initPath ?: JPATH_ADMINISTRATOR . '/components/' . $addon->element . '/include.php';
+        }
+
+        $addons = $simplerenew->params->get('addons', array());
+
+        // Prevent duplication of already registered addons
+        $idx = call_user_func(
+            function ($id, array $addons) {
+                foreach ($addons as $i => $addon) {
+                    if ($id == $addon->extension_id) {
+                        return $i;
+                    }
+                }
+                return count($addons);
+            },
+            $addon->extension_id,
+            $addons
+        );
+
+        $addons[$idx] = array(
+            'title'        => $title,
+            'extension_id' => $addon->extension_id,
+            'init'         => $initPath
+        );
+
+        $simplerenew->params->set('addons', $addons);
+
+        $simplerenew->params = $simplerenew->params->toString();
+        return $simplerenew->store();
+    }
+}
