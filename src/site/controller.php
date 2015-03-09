54628281e36ab27a55905a8b157958ae916dd185
Bill Tomczak <bill@ostraining.com>
2015-03-09 10:47:08 -0700

    Remove testing code, prep for beta 3

--- src/site/controller.php
+++ src/site/controller.php
@@ -11,15 +11,4 @@ defined('_JEXEC') or die();
 class SimplerenewController extends SimplerenewControllerBase
 {
     protected $default_view = 'subscribe';
-
-    public function test()
-    {
-        $c = SimplerenewFactory::getContainer();
-
-        echo '<pre>';
-
-        print_r($c->notify);
-
-        echo '</pre>';
-    }
 }
