fd2ebd9636a0a60cb7dc4f0d5846c10ffeb01ab7
Bill Tomczak <bill@ostraining.com>
2015-03-12 19:55:02 -0700

    Allow validation of css hidden radio buttons

--- src/media/js/validation.js
+++ src/media/js/validation.js
@@ -132,6 +132,7 @@
                 errorClass    : 'ost_error',
                 validClass    : 'ost_valid',
                 onkeyup       : false,
+                ignore        : 'input:hidden [readonly=true]',
                 rules         : {
                     password2: {
                         equalTo: '#password'
