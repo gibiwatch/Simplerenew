2ee88e75447666b489e20c7a90000a6bc6034566
Bill Tomczak <bill@ostraining.com>
2015-04-07 15:59:26 -0700

    Code commenting

--- src/site/controllers/validate.json.php
+++ src/site/controllers/validate.json.php
@@ -37,7 +37,7 @@ class SimplerenewControllerValidate extends SimplerenewControllerJson
                 $currentUser->load();
 
             } catch (Exception $e) {
-
+                // Really don't care at this point
             }
         }
 
