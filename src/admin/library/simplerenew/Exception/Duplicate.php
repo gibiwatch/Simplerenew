4152889fda9445481369b89dcaded420b7ee3c81
Bill Tomczak <bill@ostraining.com>
2015-07-17 12:52:58 -0700

    Improve error trapping for duplicate coupons

---
+++ src/admin/library/simplerenew/Exception/Duplicate.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package   Simplerenew
+ * @contact   www.simplerenew.com, support@simplerenew.com
+ * @copyright 2014-2015 Open Source Training, LLC. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+
+namespace Simplerenew\Exception;
+
+use Simplerenew\Exception;
+
+defined('_JEXEC') or die();
+
+class Duplicate extends Exception
+{
+    public function __construct($message = "", $code = 500, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
