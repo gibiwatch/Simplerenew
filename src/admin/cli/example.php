b32772dba52f148fb2acbc8fbc610b63f80b0742
Bill Tomczak <bill@ostraining.com>
2015-09-08 15:34:53 -0700

    Include example cli script

---
+++ src/admin/cli/example.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @package   Simplerenew
+ * @contact   www.simplerenew.com, support@simplerenew.com
+ * @copyright 2014-2015 Open Source Training, LLC. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+
+$options = (getopt('', array('log::')));
+
+if (isset($options['log'])) {
+    $logFile = $options['log'] ?: substr($argv[0], 0, strrpos($argv[0], '.')) . '.log';
+    if (is_file($logFile) && filesize($logFile) > (1024 * 1024)) {
+        @unlink($logFile);
+    }
+    ob_start();
+}
+
+try {
+    require_once 'bootstrap.php';
+    define('SIMPLERENEW_CLI_TASK', 'SR Example CLI - ' . date('Y-m-d H:i:s'));
+
+} catch (Exception $e) {
+    echo "\n*** " . get_class($e) . ': ' . $e->getMessage() . "\n";
+
+    if (!empty($logFile)) {
+        $buffer = ob_get_contents();
+        file_put_contents($logFile, $buffer, FILE_APPEND);
+    }
+    die;
+}
+
+class SimplerenewCliExample extends SimplerenewApplicationCli
+{
+    public function doExecute()
+    {
+        $this->timestamp('This is an example of a Simple Renew CLI script');
+    }
+}
+
+SimplerenewApplicationCli::getInstance('SimplerenewCliExample')->execute();
+
+if (!empty($logFile)) {
+    $buffer = ob_get_contents();
+    file_put_contents($logFile, $buffer, FILE_APPEND);
+}
