b25b05fe2f6b73556fa953a954bcd1c5dbc492d0
Bill Tomczak <bill@ostraining.com>
2015-10-01 14:46:47 -0700

    Reorganize for future J3 router class

--- src/site/router.php
+++ src/site/router.php
@@ -10,47 +10,90 @@ defined('_JEXEC') or die();
 
 function SimplerenewBuildRoute(array &$query)
 {
-    $segments = array();
-
-    $app = JFactory::getApplication();
-    if (empty($query['Itemid'])) {
-        $menuItem = $app->getMenu()->getActive();
-    } else {
-        $menuItem = $app->getMenu()->getItem($query['Itemid']);
-    }
-    $menuView   = isset($menuItem->query['view']) ? $menuItem->query['view'] : null;
-    $menuLayout = isset($menuItem->query['layout']) ? $menuItem->query['layout'] : null;
-
-    if (isset($query['view'])) {
-        $view = $query['view'];
-        if ($view != $menuView) {
-            $segments[] = $view;
-        }
-        unset($query['view']);
-    }
-
-    if (isset($query['layout'])) {
-        $layout = $query['layout'];
-        if ($layout != $menuLayout) {
-            $segments[] = $query['layout'];
-        }
-        unset($query['layout']);
-    }
+    $router = new SimplerenewRouter();
+    $segments = $router->build($query);
 
     return $segments;
 }
 
 function SimplerenewParseRoute($segments)
 {
-    $vars = array();
-
-    if ($menuItem = JFactory::getApplication()->getMenu()->getActive()) {
-        $vars['option'] = 'com_simplerenew';
-        $vars['view']   = $menuItem->query['view'];
-        if (isset($menuItem->query['layout'])) {
-            $vars['layout'] = $menuItem->query['layout'];
-        }
-    }
+    $router = new SimplerenewRouter();
+    $vars = $router->parse($segments);
 
     return $vars;
+}
+
+class SimplerenewRouter //extends JComponentRouterBase
+{
+    /**
+     * Build method for URLs
+     * This method is meant to transform the query parameters into a more human
+     * readable form. It is only executed when SEF mode is switched on.
+     *
+     * @param   array &$query An array of URL arguments
+     *
+     * @return  array  The URL arguments to use to assemble the subsequent URL.
+     *
+     * @since   3.3
+     */
+    public function build(&$query)
+    {
+        $segments = array();
+
+        $app = JFactory::getApplication();
+        if (empty($query['Itemid'])) {
+            $menuItem = $app->getMenu()->getActive();
+        } else {
+            $menuItem = $app->getMenu()->getItem($query['Itemid']);
+        }
+        $menuView   = isset($menuItem->query['view']) ? $menuItem->query['view'] : null;
+        $menuLayout = isset($menuItem->query['layout']) ? $menuItem->query['layout'] : null;
+
+        if (isset($query['view'])) {
+            $view = $query['view'];
+            if ($view != $menuView) {
+                $segments[] = $view;
+            }
+            unset($query['view']);
+        }
+
+        if (isset($query['layout'])) {
+            $layout = $query['layout'];
+            if ($layout != $menuLayout) {
+                $segments[] = $query['layout'];
+            }
+            unset($query['layout']);
+        }
+
+        return $segments;
+    }
+
+    /**
+     * Parse method for URLs
+     * This method is meant to transform the human readable URL back into
+     * query parameters. It is only executed when SEF mode is switched on.
+     *
+     * @param   array &$segments The segments of the URL to parse.
+     *
+     * @return  array  The URL attributes to be used by the application.
+     *
+     * @since   3.3
+     */
+    public function parse(&$segments)
+    {
+        $vars = array();
+
+        if ($menuItem = JFactory::getApplication()->getMenu()->getActive()) {
+            $vars['option'] = 'com_simplerenew';
+            $vars['view']   = $menuItem->query['view'];
+            if (isset($menuItem->query['layout'])) {
+                $vars['layout'] = $menuItem->query['layout'];
+            } elseif (!empty($segments[0])) {
+                $vars['layout'] = $segments[0];
+            }
+        }
+
+        return $vars;
+    }
 }
