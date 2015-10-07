3183ac5bd81858bc5574e5b63719ec79812bce69
Bill Tomczak <bill@ostraining.com>
2015-10-07 16:45:01 -0700

    Initial form/button for extending trial

--- src/site/views/renewal/tmpl/cancel.php
+++ src/site/views/renewal/tmpl/cancel.php
@@ -26,59 +26,87 @@ foreach ($this->subscriptions as $subscription) {
     }
 }
 
-echo SimplerenewHelper::renderModule('simplerenew_cancel_top');
-
-if ($support = $this->funnel->get('support')) :
-    echo SimplerenewHelper::renderModule('simplerenew_cancel_support');
-    ?>
-    <p><?php echo JHtml::_('link', JRoute::_('index.php?Itemid=' . $support), 'Contact Support'); ?></p>
+$heading = $this->getHeading(
+    JText::plural(
+        'COM_SIMPLERENEW_HEADING_RENEWAL_UPDATE',
+        count($this->subscriptions),
+        false
+    )
+);
+?>
+<div class="<?php echo $this->getPageClass('ost-container simplerenew-renewal'); ?>">
     <?php
-endif;
-
-if ($trials && ($extendTrial = $this->funnel->get('extendTrial'))) :
-    echo SimplerenewHelper::renderModule('simplerenew_cancel_trial');
-    ?>
-    <p><?php echo sprintf('Offer to extend trial by %s days', $extendTrial); ?></p>
-    <?php
-endif;
-
-if ($billed && ($pause = $this->funnel->get('pauseBilling'))) :
-    $now       = new SRDateTime();
-    $dateLimit = new SRDateTime();
-
-    $dateLimit->addFromUserInput($pause);
-    $dateDiff = $dateLimit->diff($now);
-
-    echo SimplerenewHelper::renderModule('simplerenew_cancel_suspend');
-    ?>
-    <p>
-        <?php
-        echo sprintf(
-            'Offer to suspend billing for as long as %s days (%s)',
-            $dateDiff->format('%a'),
-            $dateLimit->format('Y-m-d')
-        );
+    if ($heading) :
         ?>
-    </p>
-    <?php
-endif;
-
-if (($coupon = $this->funnel->get('offerCoupon'))) :
-    if ($discount = $this->getDiscount($coupon, $this->subscriptions)) :
-        echo SimplerenewHelper::renderModule('simplerenew_cancel_discount');
-        ?>
-        <p>
-            <?php
-            echo sprintf(
-                'I want to save %s on my next renewal!',
-                JHtml::_('currency.format', $discount)
-            );
-            ?>
-        </p>
+        <div class="page-header">
+            <h1><?php echo $heading; ?></h1>
+        </div>
         <?php
     endif;
-endif;
-?>
+
+    echo SimplerenewHelper::renderModule('simplerenew_cancel_top');
+
+    if ($support = $this->funnel->get('support')) :
+        echo SimplerenewHelper::renderModule('simplerenew_cancel_support');
+        ?>
+        <p><?php echo JHtml::_('link', JRoute::_('index.php?Itemid=' . $support), 'Contact Support'); ?></p>
+        <?php
+    endif;
+
+    if ($trials && ($extendTrial = $this->funnel->get('extendTrial'))) :
+        echo SimplerenewHelper::renderModule('simplerenew_cancel_trial');
+        ?>
+        <form
+            id="formExtendTrial"
+            name="formExtendTrial"
+            action="index.php"
+            method="post">
+            <button type="submit" class="btn btn-main btn-small">
+                <?php echo JText::sprintf('COM_SIMPLERENEW_CANCEL_EXTEND_TRIAL', $extendTrial); ?>
+            </button>
+        </form>
+        <?php
+    endif;
+
+    if ($billed && ($pause = $this->funnel->get('pauseBilling'))) :
+        $now      = new SRDateTime();
+        $dateLimit = new SRDateTime();
+
+        $dateLimit->addFromUserInput($pause);
+        $dateDiff = $dateLimit->diff($now);
+
+        echo SimplerenewHelper::renderModule('simplerenew_cancel_suspend');
+        ?>
+        <form
+            id="formPauseBilling"
+            name="formPauseBilling"
+            action="index.php"
+            method="post">
+            <button type="submit" class="btn btn-main btn-small">
+                <?php echo JText::sprintf('COM_SIMPLERENEW_CANCEL_PAUSE_BILLING', $pause); ?>
+            </button>
+        </form>
+        <?php
+    endif;
+
+    if (($coupon = $this->funnel->get('offerCoupon'))) :
+        if ($discount = $this->getDiscount($coupon, $this->subscriptions)) :
+            echo SimplerenewHelper::renderModule('simplerenew_cancel_discount');
+            ?>
+            <p>
+                <?php
+                echo sprintf(
+                    'I want to save %s on my next renewal!',
+                    JHtml::_('currency.format', $discount)
+                );
+                ?>
+            </p>
+            <?php
+        endif;
+    endif;
+    ?>
     <p>I'm not interested, just cancel my renewal</p>
-<?php
-echo SimplerenewHelper::renderModule('simplerenew_cancel_bottom');
+    <?php
+    echo SimplerenewHelper::renderModule('simplerenew_cancel_bottom');
+    ?>
+</div>
