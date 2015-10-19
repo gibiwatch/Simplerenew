7f0ec47bc721172fcf4f74dc18ed50ab1b88c272
Bill Tomczak <bill@ostraining.com>
2015-10-19 15:51:30 -0700

    Add a gateway selector field

---
+++ src/admin/models/fields/gateways.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package   Simplerenew
+ * @contact   www.simplerenew.com, support@simplerenew.com
+ * @copyright 2014-2015 Open Source Training, LLC. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+
+defined('_JEXEC') or die();
+
+if (!defined('SIMPLERENEW_LOADED')) {
+    require_once JPATH_ADMINISTRATOR . '/components/com_simplerenew/include.php';
+}
+
+JFormHelper::loadFieldClass('List');
+
+class SimplerenewFormFieldGateways extends JFormFieldList
+{
+    protected function getOptions()
+    {
+        $containers = SimplerenewFactory::getAllGatewayContainers();
+        $exclude    = array_map('trim', explode(',', strtolower($this->element['exclude'])));
+
+        $options = array();
+        foreach ($containers as $gateway => $container) {
+            if (!in_array(strtolower($gateway), $exclude)) {
+                $options[] = JHtml::_('select.option', $gateway, $gateway);
+            }
+        }
+
+        return array_merge(parent::getOptions(), $options);
+    }
+
+}
